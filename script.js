c6927a3ba32ae40b5694f744fd60c9368c34ab80
Nicky <nickyliu91@gmail.com>
2020-04-12 18:21:18 -0400

    added functions

--- script.js
+++ script.js
@@ -4,4 +4,43 @@ document.addEventListener('DOMContentLoaded', () => {
   var signUpButton = document.querySelector("#sign-up");
   var userEmailSpan = document.querySelector("#user-email");
   var userPasswordSpan = document.querySelector("#user-password");
+
+  // console.log(emailInput)
+  // console.log(passwordInput)
+  // console.log(signUpButton)
+  // console.log(userEmailSpan)
+  // console.log(userPasswordSpan)
+
+  function renderLastRegistered() {
+    var email = localStorage.getItem("email");
+    var password = localStorage.getItem("password");
+
+    if (email && password === null) {
+        return;
+    }
+
+    userEmailSpan.textContent = email;
+    userPasswordSpan.textContent = password;
+  }
+
+  renderLastRegistered();
+
+  signUpButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#password").value;
+
+    if (email === "") {
+        displayMessage("error", "Email cannot be blank");
+    } else if (password === "") {
+        displayMessage("error", "Password cannot be blank");
+    } else {
+        displayMessage("success", "Registered successfully");
+
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+        renderLastRegistered();
+    }
+  });
 })
