d44abdc6b9b809c3420046cafc9e5a4ad3736d57
Nicky <nickyliu91@gmail.com>
2020-04-12 18:04:00 -0400

    cleaned up index.html again

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var emailInput = document.querySelector("#email");
+  var passwordInput = document.querySelector("#password");
+  var signUpButton = document.querySelector("#sign-up");
+  var userEmailSpan = document.querySelector("#user-email");
+  var userPasswordSpan = document.querySelector("#user-password");
+})
